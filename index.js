e451339ff3ae2e016d9042c791e3db1bf1edceae
jesus alberto garcia hernandez <jesusalberto278@gmail.com>
2023-01-27 11:34:46 -0400

    example ping/pong

--- index.js
+++ index.js
@@ -4,15 +4,17 @@ const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html')
 });
 
 io.on('connection', (socket) => {
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
-  });
-});
+  console.log(socket.id)
+  
+  socket.on('ping', () => {
+    socket.emit('pong')
+  })
+})
 
 http.listen(port, () => {
-  console.log(`Socket.IO server running at http://localhost:${port}/`);
-});
+  console.log(`Socket.IO server running at http://localhost:${port}/`)
+})
